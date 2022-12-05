241ac72262ad515bf03cc9f17cb6a6593d349da4
tryit4you <hungitqnam@gmail.com>
2022-12-05 07:15:48 +0700

    F

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_app/styles.dart';
+
+final theme = ThemeData(
+  fontFamily: 'Cabin',
+  primaryColor: AppColor.midnightSky,
+  accentColor: AppColor.midnightCloud,
+  primaryTextTheme: Theme.of(context).textTheme
+
+);
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ,
+      debugShowCheckedModeBanner: false,
+      home: ,
+    )
+  }
+}
